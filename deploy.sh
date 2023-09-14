9af5d783dc903136bae5dd5487afaf3c5d5edad0
Kevin Bongcasan <kbongcasmis@gmail.com>
2023-09-14 12:35:30 -0500

    Updated deployments

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-RESOURCE_GROUP="clipdat"
+RESOURCE_GROUP="clipdat-dev" #change this in master
 LOCATION="eastus"
-STORAGE_ACCOUNT="clipdatsa"
-COSMOSDB_ACCOUNT="clipdatcsmdba"
-COSMOSDB_NAME="clipdatcmsdb"
-COSMOSDB_CONTAINER="userclips"
-QUEUE="clips"
-ENV_NAME="clipdat-env"
-SHARE_NAME="convertershare"
+STORAGE_ACCOUNT="clipdatsadev" #change this in master
+COSMOSDB_ACCOUNT="clipdatcsmdbadev"
+COSMOSDB_NAME="clipdatcsmdb"
+COSMOSDB_CONTAINER="clips"
+QUEUE_NAME="clips"
+BLOBCON_CLIPS_NAME="clips"
+BLOBCON_CONVERTED_NAME="clips"
+ENV_NAME="clipdat-dev-env"
 
 # creating resource group
 az group create \
     --name $RESOURCE_GROUP \
     --location $LOCATION
-
 
 # creating azure storage account
 az storage account create \
@@ -20,78 +20,104 @@ az storage account create \
     --resource-group $RESOURCE_GROUP \
     --location $LOCATION \
     --sku Standard_LRS \
-    --kind StorageV2
+    --kind StorageV2 \
+    --allow-blob-public-access true
 
-az storage container create  \
-    --account-name $STORAGE_ACCOUNT  \
-    --name clips
-
-az storage container create  \
-    --account-name $STORAGE_ACCOUNT  \
-    --name converted
-
-#TODO - set converted container to anon access
-#TODO - Added ephemeralstorage
-CONNECTION_STRING=$(az storage account show-connection-string \
+# Get connection string from storage account
+AZ_STORAGE_CON=$(az storage account show-connection-string \
      -g $RESOURCE_GROUP \
      --name $STORAGE_ACCOUNT \
      --out tsv)
 
+# creating azure storage account container
+az storage container create  \
+    --account-name $STORAGE_ACCOUNT  \
+    --name $BLOBCON_CLIPS_NAME
+
+# creating azure storage account container
+az storage container create  \
+    --account-name $STORAGE_ACCOUNT  \
+    --name $BLOBCON_CONVERTED_NAME
+
+# setting Public read access for blobs only
+az storage container set-permission \
+    --account-name $STORAGE_ACCOUNT  \
+    --name $BLOBCON_CONVERTED_NAME \
+    --public-access blob \
+    --connection-string $AZ_STORAGE_CON
+
 # creating azure storage queue
 az storage queue create \
-    --name $QUEUE \
+    --name $QUEUE_NAME \
     --account-name $STORAGE_ACCOUNT \
-    --connection-string $CONNECTION_STRING
+    --connection-string $AZ_STORAGE_CON
 
+# creating cosmosdb account
 az cosmosdb create \
     --name $COSMOSDB_ACCOUNT \
     --capabilities EnableServerless \
     --resource-group $RESOURCE_GROUP \
     --default-consistency-level Eventual \
-    --location regionName="$LOCATION" \
+    --location regionName="$LOCATION"
 
+# creating cosmosdb db
 az cosmosdb sql database create \
     --account-name $COSMOSDB_ACCOUNT \
     --resource-group $RESOURCE_GROUP \
-    --name $COSMOSDB_NAME \
+    --name $COSMOSDB_NAME
 
+# creating cosmosdb db container
+# -partion-key-path: double forward slash if using gitbash
 az cosmosdb sql container create \
     --account-name $COSMOSDB_ACCOUNT \
     --resource-group $RESOURCE_GROUP \
     --database-name $COSMOSDB_NAME \
     --name $COSMOSDB_CONTAINER \
-    --partition-key-path "//id" #< double forward slash if using gitbash
+    --partition-key-path "//userId" 
 
-# deploy template
-az deployment group create \
-    --resource-group $RESOURCE_GROUP \
-    --template-file clip-converter-template.json \
-    --parameters queueConnection=$CONNECTION_STRING \
-    --parameters queueName=$QUEUE \
-    --parameters storageAccountName=$STORAGE_ACCOUNT \
-    --parameters environmentName=$ENV_NAME
-
- COSMOS_DB_CONNECTION_STRING=$(az cosmosdb keys list \
+# get cosmos db connection string
+COSMOS_DB_CON=$(az cosmosdb keys list \
          -g $RESOURCE_GROUP \
          -n $COSMOSDB_ACCOUNT \
          --type connection-strings \
          --query connectionStrings[0].connectionString \
          --output tsv)
+
  
-echo $COSMOS_DB_CONNECTION_STRING
- 
- az deployment group create \
+# deploy clips service using template
+az deployment group create \
      --resource-group $RESOURCE_GROUP \
      --template-file clips-service-template.json \
-     --parameters cosmosDbConnectionString=$COSMOS_DB_CONNECTION_STRING \
+     --parameters cosmosDbConnectionString=$COSMOS_DB_CON \
      --parameters cosmosDbCosmosDbId=$COSMOSDB_NAME \
      --parameters cosmosDbUsersContainerId=$COSMOSDB_CONTAINER \
-     --parameters environmentName=$ENV_NAME
+     --parameters environmentName=$ENV_NAME \
+     --parameters blobDbConnection=$AZ_STORAGE_CON \
+     --parameters convertedContainerName=$BLOBCON_CONVERTED_NAME
 
+CLIPS_SERVICE_ENDPOINT=$(az containerapp ingress show \
+         -g $RESOURCE_GROUP \
+         -n clipsservice \
+         --query fqdn \
+         --output tsv)
+
+# deploy converter using template
+az deployment group create \
+    --resource-group $RESOURCE_GROUP \
+    --template-file clip-converter-template.json \
+    --parameters queueConnection=$AZ_STORAGE_CON \
+    --parameters queueName=$QUEUE_NAME \
+    --parameters storageAccountName=$STORAGE_ACCOUNT \
+    --parameters environmentName=$ENV_NAME \
+    --parameters clipsServiceEndpoint=$CLIPS_SERVICE_ENDPOINT
+
+ 
+# deploy uploader using template
 az deployment group create \
     --resource-group $RESOURCE_GROUP \
     --template-file clip-uploader-template.json \
-    --parameters connectionString=$CONNECTION_STRING \
-    --parameters queueName=$QUEUE \
-    --parameters blobContainerName="clips" \
-    --parameters environmentName=$ENV_NAME
+    --parameters connectionString=$AZ_STORAGE_CON \
+    --parameters queueName=$QUEUE_NAME \
+    --parameters blobContainerName=$BLOBCON_CLIPS_NAME \
+    --parameters environmentName=$ENV_NAME \
+    --parameters clipsServiceEndpoint=$CLIPS_SERVICE_ENDPOINT
